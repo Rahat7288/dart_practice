fd2a05a58c514e92a8729b3ff657869b6826ca37
rahat <rahatislamakash@gmail.com>
2021-06-12 15:59:03 +0600

    ganer line liksilam dada

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) {
+  print('Hello world!');
+  print("trying something new jin my life");
+  print("koronio new kaj kam ");
+
+  //print line shiktasi r ki!
+  print("mago ojhor borshay mishe jabo tomar ");
+}
